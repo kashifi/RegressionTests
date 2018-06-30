3f02398074eb6f50449179982472016953424d09
kashifi <kashif.iq82@gmail.com>
2018-06-30 20:08:59 +1200

    Emailer Class removed temporarily

--- src/main/java/com/engcpp/utils/Constants.java
+++ src/main/java/com/engcpp/utils/Constants.java
@@ -7,18 +7,10 @@ package com.engcpp.utils;
 public class Constants {
 	
 	/** Parameters to initiate for the CTA environment  **/	
-    /*
+    
 	public static final String IQC_URL = "https://iqconnect.test.equifax.co.nz";
     public static final String USERNAME = "kashifi";
     public static String PASSWORD = "Equifax@01";
     public static String NEW_PASSWORD;	    
-    
-    */
-        
-    /** Parameters to initiate for the QA environment **/ 
-	
-	public static final String IQC_URL = "https://iqconnect.qa.nz.corp.bayadv";
-    public static final String USERNAME = "16929qa0003";
-    public static final String PASSWORD = "Riyadh@01";
     
 }
